6d7f26c20049e2a59a4373a78cd988e6c935a14f
Jacob <jkohler06@icloud.com>
2026-04-13 11:47:35 -0500

    Brought in debounce

---
+++ debounce.sv
@@ -0,0 +1,24 @@
+module debounce (
+	input logic clk,
+	input logic rst,
+	input logic button_in,
+	output logic button_clean
+	);
+	logic [7:0] shift;
+
+	always_ff @ ( posedge clk or posedge rst ) begin
+		if (rst) begin
+			shift <= 0;
+			button_clean <= 0;
+		end else begin
+			shift <= {shift[6:0], button_in};
+
+			if (&shift)
+				button_clean <= 1;
+			else if (~|shift)
+				button_clean <= 0;
+		end
+	end
+
+endmodule
+
