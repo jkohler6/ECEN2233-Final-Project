a689b2b0274a708921c365c44652ecee8df1e1dc
IsaacGarven <isaacg12321@gmail.com>
2026-04-13 12:52:01 -0500

    Finished top part of fsm

--- thunderbird_fsm.sv
+++ thunderbird_fsm.sv
@@ -8,24 +8,33 @@ FSM goes to IDLE
 Left Turn
 Left lights flash
 Continues even if button is released
-Right turn is the same
 
+Right turn
+same as left turn
 
+Hazard Mode:
+Right and left stuck together
+Activates if L and R are both pressed
+
+Brake Mode:
+All lights ON
+Lights stay ON while brake is active
+
+Priority:
+Brake > Hazard > Turn Signals (Left/Right)
 */
 
 module fsm (
-	input logic rst,
+	input logic clk,
+	// *removed until we need it again, if something breaks because rst is missing, uncomment this line
+	// input logic rst,
 	input logic left,
 	input logic right,
-    input logic right,
-    output logic mode,
-	output logic [2:0] sev
+    input logic brake,
+	output logic [3:0] state
 );
-	always_ff @ (posedge clk) begin
-		if (rst)
-			q <= 8'b0;
-		else if (en)
-			q <= q + 1;
-	end
+
+
+
 
 endmodule
