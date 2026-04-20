ac4aeda9948df0e4a60880e7a80692b9c52485a4
Jacob <jkohler06@icloud.com>
2026-04-20 11:48:34 -0500

    Updated FSM

--- thunderbird_fsm.sv
+++ thunderbird_fsm.sv
@@ -25,16 +25,42 @@ Brake > Hazard > Turn Signals (Left/Right)
 */
 
 module fsm (
-	input logic clk,
 	// *removed until we need it again, if something breaks because rst is missing, uncomment this line
-	// input logic rst,
+	input logic clk,
+	input logic rst,
+	input logic en,
 	input logic left,
 	input logic right,
-    input logic brake,
-	output logic [3:0] state
+    	input logic brake,
+	output logic [5:0] lights,
+	output logic [3:0] mode
 );
 
+typedef enum logic [3:0] {
+	ST_IDLE,
+	ST_L1,
+	ST_L2,
+	ST_L3,
+	ST_R1,
+	ST_R2,
+	ST_R3,
+	H1_ST,
+	H2_ST,
+	H3_ST,
+	B_ST
+}state_t;
 
+state_t current, next;
 
+always_ff @(posedge clk or posedge rst) begin
+	if(reset)
+		current <= ST_IDLE;
+	else if (en)
+		current <= next;
+end
 
+always_comb begin
+	next = current;
+
+	unique case (current)
 endmodule
