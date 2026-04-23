d584ca3d2fcc52a0876610ebdec466d7c56d6722
Jacob <jkohler06@icloud.com>
2026-04-23 09:42:03 -0500

    Sync

--- thunderbird_fsm.sv
+++ thunderbird_fsm.sv
@@ -1,77 +1,29 @@
-/*
-Modes
-
-Reset Mode:
-All lights OFF
-FSM goes to IDLE
-
-Left Turn
-Left lights flash
-Continues even if button is released
-
-Right turn
-same as left turn
-
-Hazard Mode:
-Right and left stuck together
-Activates if L and R are both pressed
-
-Brake Mode:
-All lights ON
-Lights stay ON while brake is active
-
-Priority:
-Brake > Hazard > Turn Signals (Left/Right)
-*/
-
 module fsm (
 	input logic clk,
 	input logic rst,
-	input logic preempt,
 	input logic [3:0] ctrl,
 	output logic [5:0] out,
 	output logic [3:0] mode
 );
 
-typedef struct packed {
-	logic	brake,
-	logic   L,
-	logic	R,
-	logic	H
-} ctrl_t;
+/* Revaluate the need for a control word struct */
 
 typedef enum logic [5:0] {
-	ST_IDLE = 3'b000000,
-	ST_L1   = 3'b001000,
-	ST_L2	= 3'b011000,
-	ST_L3   = 3'b111000,
-	ST_R1	= 3'b000100,
-	ST_R2	= 3'b000110,
-	ST_R3	= 3'b000111,
-	H1_ST	= 3'b001100,
-	H2_ST	= 3'b011110,
-	H3_ST	= 3'b111111,
-	B_ST	= 3'b111111
+	ST_IDLE = 6'b000000,
+	ST_L1   = 6'b001000,
+	ST_L2	= 6'b011000,
+	ST_L3   = 6'b111000,
+	ST_R1	= 6'b000100,
+	ST_R2	= 6'b000110,
+	ST_R3	= 6'b000111,
+	ST_H1	= 6'b001100,
+	ST_H2	= 6'b011110,
+	ST_H3	= 6'b111111,
+	ST_B	= 6'b111111
 } state_t;
 
 state_t current, next;
 ctrl_t ctrl;
-
-always_comb begin
-	unique case (current)
-		ST_IDLE: ctrl = '{};
-		ST_L1:	 ctrl = '{next:ST_L2};
-		ST_L2:	 ctrl = '{next:ST_L3};
-		ST_L3:   ctrl = '{next:ST_IDLE};
-		ST_R1:   ctrl = '{next:ST_R2};
-		ST_R2:   ctrl = '{next:ST_R3};
-		ST_R3:   ctrl = '{next:ST_IDLE};
-		H1_ST:   ctrl = '{next:ST_H2};
-		H2_ST:   ctrl = '{next:ST_H3};
-		H3_ST:   ctrl = '{next:ST_IDLE};
-		B_ST:    ctrl = '{next:ST_B};
-	endcase
-end
 
 assign next = state_t'(ctrl.next);
 
@@ -79,7 +31,23 @@ always_ff @(posedge clk or posedge rst) begin
 	if (reset)
 		current <= ST_IDLE;
 	else
-		current <= preempt ? B_ST : next;
+		current <= (ctrl == 4'b1111 ) ? ST_B : next;
+end
+
+always_comb begin
+	unique case (current)
+		ST_IDLE: ctrl = '{mode:b'0000, next:ST_IDLE};
+		ST_L1:	 ctrl = '{mode:b'0001, next:ST_L2};
+		ST_L2:	 ctrl = '{mode:b'0001, next:ST_L3};
+		ST_L3:   ctrl = '{mode:b'0001, next:ST_IDLE};
+		ST_R1:   ctrl = '{mode:b'0010, next:ST_R2};
+		ST_R2:   ctrl = '{mode:b'0010, next:ST_R3};
+		ST_R3:   ctrl = '{mode:b'0010, next:ST_IDLE};
+		ST_H1:   ctrl = '{mode:b'0100, next:ST_H2};
+		ST_H2:   ctrl = '{mode:b'0100, next:ST_H3};
+		ST_H3:   ctrl = '{mode:b'0100, next:ST_IDLE};
+		ST_B:    ctrl = '{mode:b'1111, next:ST_B};
+	endcase
 end
 
 assign out = current;
